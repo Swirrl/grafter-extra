81ad6bac8b01f82c71c4b3e4675c46b1f4558dcf
Robin Gower <robin.git@infonomics.ltd.uk>
2015-07-30 17:13:21 +0100

    Correct with-repository test so it uses a connection

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [grafter/grafter "0.5.0"]
-                 [grafter/vocabularies "0.1.1-SNAPSHOT"]]
+                 [grafter/vocabularies "0.1.3-SNAPSHOT"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]])
--- test/grafter/extra/repository_test.clj
+++ test/grafter/extra/repository_test.clj
@@ -1,14 +1,16 @@
 (ns grafter.extra.repository-test
   (:require [clojure.test :refer :all]
             [grafter.extra.repository :refer [with-repository]]
-            [grafter.rdf.repository :refer [repo query]]
-            [grafter.rdf.protocols :refer [->Quad add]]))
+            [grafter.rdf.repository :refer [repo query ->connection]]
+            [grafter.rdf.protocols :refer [->Quad add]])
+  (:import [org.openrdf.repository RepositoryConnection]))
 
 (deftest with-repository-test
   (testing "binds a repository"
     (with-repository [test-repo (repo)]
-      (let [input (->Quad "http://s" "http://p" "http://o" "http://g")]
-        (add test-repo input)
-        (let [output (query test-repo "SELECT * WHERE { ?s ?p ?o }")]
-          (is (= 1
-                 (count output))))))))
+      (with-open [connection (->connection test-repo)]
+        (let [input (->Quad "http://s" "http://p" "http://o" "http://g")]
+          (add connection input)
+          (let [output (query connection "SELECT * WHERE { ?s ?p ?o }")]
+            (is (= 1
+                   (count output)))))))))
