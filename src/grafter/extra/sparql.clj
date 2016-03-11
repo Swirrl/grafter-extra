fb16bcf45f584fa9615855527b84973ca734578a
Robin Gower <robin.git@infonomics.ltd.uk>
2016-03-11 13:56:23 +0100

    More sparql building functions

--- src/grafter/extra/sparql.clj
+++ src/grafter/extra/sparql.clj
@@ -19,11 +19,46 @@
   ([subject predicate object]
    (str subject " " predicate " " object " .")))
 
-(def join-space
-  (partial string/join " "))
+(defn join-comma [& args]
+  (string/join ", " args))
 
-(def join-newline
-  (partial string/join "\n"))
+(defn join-space [& args]
+  (string/join " " args))
+
+(defn join-newline [& args]
+  (string/join "\n" args))
+
+(defn prefix-space [arg]
+  (str "  " arg))
+
+(defn indent [arg]
+  (map prefix-space arg))
 
 (defn sparql-uri [uri]
   (str "<" uri ">"))
+
+(def to-uri sparql-uri)
+
+(defn object-list [obj-or-list]
+  (if (or (vector? obj-or-list) (seq? obj-or-list))
+    (apply join-comma obj-or-list)
+    obj-or-list))
+
+(defn verb-object-list [[verb obj-list]]
+  (str (join-space verb (object-list obj-list)) ";"))
+
+(defn property-list [prop-list]
+  (map verb-object-list prop-list))
+
+(defn triples-same-subject [subj prop-list]
+  (concat [subj]
+          (indent (property-list prop-list))
+          ["  ."]))
+
+(defn group-graph-pattern [subj prop-list]
+  (apply join-newline
+         (concat ["{"]
+                 (indent (triples-same-subject subj prop-list))
+                 ["}"])))
+
+(def ggp group-graph-pattern)
--- test/grafter/extra/sparql_test.clj
+++ test/grafter/extra/sparql_test.clj
@@ -6,3 +6,49 @@
   (is (= '("?a" "?b" "?c" "?d")
          (alphabetical-vars-for (range 4)))))
 
+
+(deftest object-list-test
+  (testing "long object"
+    (is (= "obj"
+           (object-list "obj"))))
+  (testing "list of objects"
+    (is (= "obj-a, obj-b, obj-c"
+           (object-list ["obj-a" "obj-b" "obj-c"])))))
+
+(deftest verb-object-list-test
+  (testing "predicate with object"
+    (is (= "prop obj;"
+           (verb-object-list ["prop" "obj"]))))
+  (testing "predicate with list of objects"
+    (is (= "prop obj-a, obj-b, obj-c;"
+           (verb-object-list ["prop" ["obj-a" "obj-b" "obj-c"]])))))
+
+(deftest property-list-test
+  (is (= ["prop1 obj1;"
+          "prop2 obj2;"]
+         (property-list [["prop1" "obj1"]
+                         ["prop2" "obj2"]]))))
+
+(deftest triples-same-subject-test
+  (is (= ["subj"
+          "  prop1 obj1;"
+          "  prop2 obj2-a, obj2-b;"
+          "  ."]
+         (triples-same-subject "subj"
+                               [["prop1" "obj1"]
+                                ["prop2" ["obj2-a" "obj2-b"]]]))))
+
+(defn long-str [& strings] (clojure.string/join "\n" strings))
+
+(deftest group-graph-pattern-test
+  (is (= (long-str
+          "{"
+          "  s"
+          "    p1 o1;"
+          "    p2 o2;"
+          "    ."
+          "}") ;; "{\n  s\n    p1 o1;\n    p2 o2;\n    .\n}"
+         (group-graph-pattern
+          "s"
+          [["p1" "o1"]
+           ["p2" "o2"]]))))
