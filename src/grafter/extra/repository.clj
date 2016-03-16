b446052c3bd44208007cb6660bd2df9ab35acaf2
Robin Gower <robin.git@infonomics.ltd.uk>
2016-03-16 15:32:12 +0100

    repository/with-repository-containing

--- src/grafter/extra/repository.clj
+++ src/grafter/extra/repository.clj
@@ -1,8 +1,22 @@
 (ns grafter.extra.repository
-  (:require [grafter.rdf.repository :refer [shutdown]]))
+  (:require [grafter.rdf :refer [add statements]]
+            [grafter.rdf.repository :refer [shutdown repo ->connection]]))
 
 (defmacro with-repository [repo-binding & body]
   `(let ~repo-binding
      (try
        ~@body
        (finally (shutdown (first ~repo-binding))))))
+
+(defn load-contents [repository contents]
+  (let [is-file? string?
+        data (if (is-file? contents) (statements contents) contents)]
+    (with-open [connection (->connection repository)]
+      (add connection data))))
+
+(defmacro with-repository-containing [[repo-binding repo-contents] & body]
+  `(let [~repo-binding (repo)]
+     (try
+       (load-contents ~repo-binding ~repo-contents)
+       ~@body
+       (finally (shutdown ~repo-binding)))))
--- test/grafter/extra/repository_test.clj
+++ test/grafter/extra/repository_test.clj
@@ -1,6 +1,6 @@
 (ns grafter.extra.repository-test
   (:require [clojure.test :refer :all]
-            [grafter.extra.repository :refer [with-repository]]
+            [grafter.extra.repository :refer :all]
             [grafter.rdf.repository :refer [repo query ->connection]]
             [grafter.rdf.protocols :refer [->Quad add]])
   (:import [org.openrdf.repository RepositoryConnection]))
@@ -14,3 +14,19 @@
           (let [output (query connection "SELECT * WHERE { ?s ?p ?o }")]
             (is (= 1
                    (count output)))))))))
+
+(deftest with-repository-containing-test
+  (testing "binds an in-memory repository, adding data to it"
+    (letfn [(repo-loaded-with? [data]
+              (with-repository-containing [test-repo data]
+                (with-open [connection (->connection test-repo)]
+                  (let [anything-there? "ASK { ?s ?p ?o }"]
+                    (query connection anything-there?)))))]
+
+      (testing "from a sequence of statements"
+        (let [data (list (->Quad "http://s" "http://p" "http://o" "http://g"))]
+          (is (repo-loaded-with? data))))
+
+      (testing "from a file"
+        (let [data "./test/resources/example.ttl"]
+          (is (repo-loaded-with? data)))))))
