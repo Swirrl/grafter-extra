dcf3637d60ec1a8410932d753f7632d55f1cf03b
Robin Gower <robin.git@infonomics.ltd.uk>
2016-03-16 17:05:49 +0100

    Functions for processing and testing sequences of statements

--- src/grafter/extra/statements.clj
+++ src/grafter/extra/statements.clj
@@ -1,4 +1,5 @@
-(ns grafter.extra.statements)
+(ns grafter.extra.statements
+  (:require [grafter.vocabularies.rdf :refer [rdf:a]]))
 
 (defn statements-where [attr-map statements]
   "Filters a sequence of statements according to an attribute map
@@ -11,3 +12,20 @@
             (every? (fn [[part value]] (= (get quad part) value))
                     attr-map))
           statements))
+
+(defn quad-is-a? [rdf-type]
+  "Matches quads with `?s a <rdf-type>`"
+  (fn [[s p o g]]
+    (and (= p rdf:a)
+         (= o rdf-type))))
+
+(defn unique-uris-of-type [rdf-type statements]
+  "Finds unique subjects where `?s a <rdf-type>`"
+  (->> statements
+       (filter (quad-is-a? rdf-type))
+       (map :s)
+       distinct))
+
+(defn count-of-type? [rdf-type statements]
+  "Counts unique subjects where `?s a <rdf-type>`"
+  (count (unique-uris-of-type rdf-type statements)))
