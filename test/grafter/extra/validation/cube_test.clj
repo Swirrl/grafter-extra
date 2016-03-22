ed893f0a0c2e0cc9358abbd318403a821eeb6fdc
Robin Gower <robin.git@infonomics.ltd.uk>
2016-03-22 14:16:06 +0100

    load sdmx-dimensions prerequisite

--- test/grafter/extra/validation/cube_test.clj
+++ test/grafter/extra/validation/cube_test.clj
@@ -1,17 +1,20 @@
 (ns grafter.extra.validation.cube-test
   (:require [clojure.test :refer :all]
             [grafter.rdf.repository :refer [repo]]
-            [grafter.extra.repository :refer [with-repository-containing]]
+            [grafter.extra.repository :refer [with-repository-containing
+                                              load-contents]]
             [grafter.extra.validation.cube :refer :all]))
 
 (deftest assert-well-formed-test
   (testing "Nothing thrown with well-formed cubes"
     (with-repository-containing [r "./test/resources/cube.ttl"]
+      (load-contents r "./test/resources/sdmx-dimension.ttl")
       (normalise-cube r)
       (assert-well-formed r)))
 
   (testing "Errors thrown with badly-formed cube"
     (with-repository-containing [r "./test/resources/cube-bad.ttl"]
+      (normalise-cube r)
       (is (thrown-with-msg? AssertionError
                             #"Malformed Cube"
                             (assert-well-formed r))))))
