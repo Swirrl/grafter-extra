c1b2cefb961c2ceb9efb6789dde32adce242802f
Robin Gower <robin.git@infonomics.ltd.uk>
2016-04-08 20:03:00 +0200

    More checks and omissions

--- src/grafter/extra/validation/pmd.clj
+++ src/grafter/extra/validation/pmd.clj
@@ -2,23 +2,41 @@
   (:require [grafter.extra.validation :refer [presence-checker]]
             [grafter.extra.sparql :as sparql]))
 
-(defn add-error [errors error]
-  (conj errors error))
-
 (defn template [filename]
   (sparql/template (str "validation/pmd/" filename)))
 
+(defn checker [filename dataset-uri message]
+  (presence-checker ((template filename) {:dataset-uri dataset-uri})
+                    message))
+
 (defn check-for-pmd-dataset [dataset-uri]
-  (presence-checker ((template "ds-a-pmd-dataset.sparql") {:dataset-uri dataset-uri})
-                    "is not a pmd:Dataset"))
+  (checker "ds-a-pmd-dataset.sparql" dataset-uri "is not a pmd:Dataset"))
+
+(defn check-for-pmd-graph [dataset-uri]
+  (checker "ds-has-pmd-graph.sparql" dataset-uri  "is missing a pmd:graph"))
+
+(defn check-for-title [dataset-uri]
+  (checker "ds-has-title.sparql" dataset-uri  "is missing a dcterms:title"))
+
+(defn check-for-modified [dataset-uri]
+  (checker "ds-has-modified.sparql" dataset-uri "is missing a dcterms:modified"))
+
+(defn add-error [errors error]
+  (conj errors error))
 
 (defn errors [repo dataset-uri]
   (let [apply-check (fn [check] ((check dataset-uri) repo))
-        checks (list check-for-pmd-dataset)]
+        checks (list check-for-pmd-dataset
+                     check-for-pmd-graph
+                     check-for-title)]
     (->> checks (map apply-check) (remove nil?))))
 
 
-;;(defn add-omission [omissions omission])
+(defn add-omission [omissions omission]
+  (conj omissions omission))
 
-;;(defn omissions [repo dataset-uri])
+(defn omissions [repo dataset-uri]
+  (let [apply-check (fn [check] ((check dataset-uri) repo))
+        checks (list check-for-modified)]
+    (->> checks (map apply-check) (remove nil?))))
 
--- test/grafter/extra/validation/pmd_test.clj
+++ test/grafter/extra/validation/pmd_test.clj
@@ -7,8 +7,10 @@
   (some (partial re-find (re-pattern string)) strings))
 
 (deftest errors-test
-  (let [expected (list "is not a pmd:Dataset")
-        invalid (with-repository-containing [r "./test/resources/cube.ttl"]
+  (let [expected (list "is not a pmd:Dataset"
+                       "is missing a pmd:graph"
+                       "is missing a dcterms:title")
+        invalid (with-repository-containing [r "./test/resources/cube-bad.ttl"]
                   (doall (errors r "http://example.org/ns#dataset-le3")))
         valid (with-repository-containing [r "./test/resources/outdoor-visits.nt"]
                 (doall (errors r "http://statistics.gov.scot/data/outdoor-visits")))]
@@ -16,9 +18,13 @@
       (is (includes? invalid message))
       (is (not (includes? valid message))))))
 
-
-
-
-
-;;(deftest omissions-test)
+(deftest omissions-test
+  (let [expected (list "is missing a dcterms:modified")
+        missing (with-repository-containing [r "./test/resources/cube-bad.ttl"]
+                  (doall (omissions r "http://example.org/ns#dataset-le3")))
+        present (with-repository-containing [r "./test/resources/outdoor-visits.nt"]
+                  (doall (omissions r "http://statistics.gov.scot/data/outdoor-visits")))]
+    (doseq [message expected]
+      (is (includes? missing message))
+      (is (not (includes? present message))))))
 
