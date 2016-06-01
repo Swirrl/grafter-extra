5b86d6ccd0673121c5d27339cd4978714f3e8b51
Rick Moynihan <rick.m@swirrl.com>
2016-06-01 11:23:38 +0100

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [grafter/grafter "0.7.1-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [grafter/grafter "0.7.2"]
                  [grafter/vocabularies "0.1.3"]
                  [selmer "1.0.4"]]
   :deploy-repositories [["releases" :clojars]
