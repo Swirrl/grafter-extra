1c6f52d15eb1aa725093c069206dd462a2238d0a
Rick Moynihan <rick.m@swirrl.com>
2017-03-06 18:12:58 +0000

    Bump version to 0.2.0-SNAPSHOT (formerly grafter 0.8.6 compat)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/extra "0.1.5-grafter080-SNAPSHOT"
+(defproject grafter/extra "0.2.0-SNAPSHOT"
   :description "A collection of extra transformations and tools for developing Grafter pipelines"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
