4525bdcd65dfc019c81a1eb2a58aa78780a2b4a6
Robin Gower <robin.git@infonomics.ltd.uk>
2017-04-19 18:32:23 +0200

    Introduce cube/errors which gathers examples

--- src/grafter/extra/validation/cube.clj
+++ src/grafter/extra/validation/cube.clj
@@ -1,128 +1,105 @@
 (ns grafter.extra.validation.cube
-  (:require
-   [clojure.java.io :as io]
-   [grafter.extra.validation :refer [absence-asserter]]
-   [grafter.rdf.repository :refer [->connection]]
-   [grafter.rdf.protocols :refer [update!]]))
-
-
-;; Well-formed cubes
-;; https://www.w3.org/TR/vocab-data-cube/#wf
-
+  (:require [clojure.java.io :as io]
+            [grafter.extra.validation :refer [absence-asserter example-finder]]
+            [grafter.rdf
+             [protocols :refer [update!]]
+             [repository :refer [->connection]]]))
 
 (defn cube-resource [filename]
   (->> filename (str "validation/cube/") io/resource slurp))
 
-(defn query-resource [filename]
+(defn ask-query [filename]
   (str (cube-resource "prefixes.sparql")
-       (cube-resource filename)))
+       (cube-resource (str "ask/" filename))))
 
-(defn cube-asserter [filename message]
-  (absence-asserter (query-resource filename) (str "Malformed Cube. " message)))
+(defn select-query [filename]
+  (str (cube-resource "prefixes.sparql")
+       (cube-resource (str "select/" filename))))
 
-(def assert-unique-dataset
-  (cube-asserter
-   "unique-dataset.sparql"
-   "Every qb:Observation has exactly one associated qb:DataSet."))
 
-(def assert-unique-dsd
-  (cube-asserter
+;; Abbreviated and Normalised data cubes
+;; https://www.w3.org/TR/vocab-data-cube/#h2_normalize
+(defn normalise-cube [repository]
+  (with-open [connection (->connection repository)]
+    (doseq [update-query ["normalise/type-and-property-closure.sparql"
+                          "normalise/push-down-attachment-levels.sparql"]]
+      (update! connection (cube-resource update-query)))))
+
+
+;; Well-formed cubes
+;; https://www.w3.org/TR/vocab-data-cube/#wf
+(def query->message
+  {"unique-dataset.sparql"
+   "Every qb:Observation has exactly one associated qb:DataSet."
+
    "unique-dsd.sparql"
-   "Every qb:DataSet has exactly one associated qb:DataStructureDefinition."))
+   "Every qb:DataSet has exactly one associated qb:DataStructureDefinition."
 
-(def assert-dsd-includes-measures
-  (cube-asserter
    "dsd-includes-measures.sparql"
-   "Every qb:DataStructureDefinition must include at least one declared measure."))
+   "Every qb:DataStructureDefinition must include at least one declared measure."
 
-(def assert-dimensions-have-range
-  (cube-asserter
    "dimensions-have-range.sparql"
-   "Every dimension declared in a qb:DataStructureDefinition must have a declared rdfs:range. "))
+   "Every dimension declared in a qb:DataStructureDefinition must have a declared rdfs:range. "
 
-(def assert-concept-dimensions-have-codelists
-  (cube-asserter
    "concept-dimensions-have-codelists.sparql"
-   "Every dimension with range skos:Concept must have a qb:codeList."))
+   "Every dimension with range skos:Concept must have a qb:codeList."
 
-(def assert-only-attributes-optional
-  (cube-asserter
    "only-attributes-optional.sparql"
-   "The only components of a qb:DataStructureDefinition that may be marked as optional, using qb:componentRequired are attributes."))
+   "The only components of a qb:DataStructureDefinition that may be marked as optional, using qb:componentRequired are attributes."
 
-(def assert-slice-key-declared
-  (cube-asserter
    "slice-key-declared.sparql"
-   "Every qb:SliceKey must be associated with a qb:DataStructureDefinition."))
+   "Every qb:SliceKey must be associated with a qb:DataStructureDefinition."
 
-(def assert-slice-key-consistent
-  (cube-asserter
    "slice-key-consistent.sparql"
-   "Every qb:componentProperty on a qb:SliceKey must also be declared as a qb:component of the associated qb:DataStructureDefinition."))
+   "Every qb:componentProperty on a qb:SliceKey must also be declared as a qb:component of the associated qb:DataStructureDefinition."
 
-(def assert-unique-slice-structure
-  (cube-asserter
    "unique-slice-structure.sparql"
-   "Each qb:Slice must have exactly one associated qb:sliceStructure."))
+   "Each qb:Slice must have exactly one associated qb:sliceStructure."
 
-(def assert-slice-dimensions-complete
-  (cube-asserter
    "slice-dimensions-complete.sparql"
-   "Every qb:Slice must have a value for every dimension declared in its qb:sliceStructure."))
+   "Every qb:Slice must have a value for every dimension declared in its qb:sliceStructure."
 
-(def assert-all-dimensions-required
-  (cube-asserter
    "all-dimensions-required.sparql"
-   "Every qb:Observation has a value for each dimension declared in its associated qb:DataStructureDefinition."))
+   "Every qb:Observation has a value for each dimension declared in its associated qb:DataStructureDefinition."
 
-(def assert-no-duplicate-observations
-  (cube-asserter
    "no-duplicate-observations.sparql"
-   " No two qb:Observations in the same qb:DataSet may have the same value for all dimensions."))
+   " No two qb:Observations in the same qb:DataSet may have the same value for all dimensions."
 
-(def assert-required-attributes
-  (cube-asserter
    "required-attributes.sparql"
-   "Every qb:Observation has a value for each declared attribute that is marked as required."))
+   "Every qb:Observation has a value for each declared attribute that is marked as required."
 
-(def assert-all-measures-present
-  (cube-asserter
    "all-measures-present.sparql"
-   "In a qb:DataSet which does not use a Measure dimension then each individual qb:Observation must have a value for every declared measure."))
+   "In a qb:DataSet which does not use a Measure dimension then each individual qb:Observation must have a value for every declared measure."
 
-(def assert-measures-dimension-consistent
-  (cube-asserter
    "measures-dimension-consistent.sparql"
-   "In a qb:DataSet which uses a Measure dimension then each qb:Observation must have a value for the measure corresponding to its given qb:measureType."))
+   "In a qb:DataSet which uses a Measure dimension then each qb:Observation must have a value for the measure corresponding to its given qb:measureType."
 
-(def assert-single-measure-on-measure-dimension-observation
-  (cube-asserter
    "single-measure-on-measure-dimension-observation.sparql"
-   "In a qb:DataSet which uses a Measure dimension then each qb:Observation must only have a value for one measure (by IC-15 this will be the measure corresponding to its qb:measureType)."))
+   "In a qb:DataSet which uses a Measure dimension then each qb:Observation must only have a value for one measure (by IC-15 this will be the measure corresponding to its qb:measureType)."
 
-(def assert-all-measures-present-in-measures-dimension-cube
-  (cube-asserter
    "all-measures-present-in-measures-dimension-cube.sparql"
-   "In a qb:DataSet which uses a Measure dimension then if there is a Observation for some combination of non-measure dimensions then there must be other Observations with the same non-measure dimension values for each of the declared measures."))
+   "In a qb:DataSet which uses a Measure dimension then if there is a Observation for some combination of non-measure dimensions then there must be other Observations with the same non-measure dimension values for each of the declared measures."
 
-(def assert-consistent-dataset-links
-  (cube-asserter
    "consistent-dataset-links.sparql"
-   "If a qb:DataSet D has a qb:slice S, and S has an qb:observation O, then the qb:dataSet corresponding to O must be D."))
+   "If a qb:DataSet D has a qb:slice S, and S has an qb:observation O, then the qb:dataSet corresponding to O must be D."
 
-(def assert-codes-from-codelist-concept-scheme
-  (cube-asserter
    "codes-from-codelist-concept-scheme.sparql"
-   "If a dimension property has a qb:codeList, then the value of the dimension property on every qb:Observation must be in the code list."))
+   "If a dimension property has a qb:codeList, then the value of the dimension property on every qb:Observation must be in the code list."
 
-(def assert-codes-from-codelist-collection
-  (cube-asserter
    "codes-from-codelist-collection.sparql"
-   "If a dimension property has a qb:codeList, then the value of the dimension property on every qb:Observation must be in the code list."))
+   "If a dimension property has a qb:codeList, then the value of the dimension property on every qb:Observation must be in the code list."})
 
 ;; not yet implmented tests for "Codes from hierarchy" and
 ;; "Codes from hierarchy (inverse)" as the ask queries need to be parametised
 
+
+
+(defn cube-asserter [filename message]
+  (absence-asserter (ask-query filename)
+                    (str "Malformed Cube. " message)))
+
+(defn cube-example-finder [filename message]
+  (example-finder (select-query filename) (str "Malformed Cube. " message)))
 
 (defn assert-well-formed [repository]
   "Asserts each cube integrity constraints in sequence, throwing at first
@@ -132,32 +109,13 @@
    succeed (as it possesses no badly-formed cubes).
 
    Well-formed cube specification: https://www.w3.org/TR/vocab-data-cube/#wf."
-  (doseq [assertion (list assert-unique-dataset
-                          assert-unique-dsd
-                          assert-dsd-includes-measures
-                          assert-dimensions-have-range
-                          assert-concept-dimensions-have-codelists
-                          assert-only-attributes-optional
-                          assert-slice-key-declared
-                          assert-slice-key-consistent
-                          assert-unique-slice-structure
-                          assert-slice-dimensions-complete
-                          assert-all-dimensions-required
-                          assert-no-duplicate-observations
-                          assert-all-measures-present
-                          assert-measures-dimension-consistent
-                          assert-single-measure-on-measure-dimension-observation
-                          assert-all-measures-present-in-measures-dimension-cube
-                          assert-consistent-dataset-links
-                          assert-codes-from-codelist-concept-scheme
-                          assert-codes-from-codelist-collection)]
-    (assertion repository)))
+  (doseq [[qry msg] query->message]
+    ((cube-asserter qry msg) repository)))
 
-
-;; Abbreviated and Normalised data cubes
-;; https://www.w3.org/TR/vocab-data-cube/#h2_normalize
-(defn normalise-cube [repository]
-  (with-open [connection (->connection repository)]
-    (doseq [update-query ["type-and-property-closure.sparql"
-                        "push-down-attachment-levels.sparql"]]
-      (update! connection (cube-resource update-query)))))
+(defn errors [repository]
+  "Validate that any cube resources are well-formed returning an error message for
+   each constraint that fails."
+  (doall
+   (remove nil?
+           (for [[qry msg] query->message]
+             ((cube-example-finder qry msg) repository)))))
--- test/grafter/extra/validation/cube_test.clj
+++ test/grafter/extra/validation/cube_test.clj
@@ -18,3 +18,18 @@
       (is (thrown-with-msg? AssertionError
                             #"Malformed Cube"
                             (assert-well-formed r))))))
+
+(deftest errors-test
+  (testing "Nothing thrown with well-formed cubes"
+    (with-repository-containing [r "./test/resources/cube.ttl"]
+      (load-contents r "./test/resources/sdmx-dimension.ttl")
+      (normalise-cube r)
+      (is (empty? (errors r))))))
+
+(testing "Errors thrown with badly-formed cube"
+  (with-repository-containing [r "./test/resources/cube-bad.ttl"]
+    (normalise-cube r)
+    (is (= '("Malformed Cube. Every dimension declared in a qb:DataStructureDefinition must have a declared rdfs:range. , e.g. http://example.org/ns#dim1"
+             "Malformed Cube. Every qb:Observation has a value for each dimension declared in its associated qb:DataStructureDefinition., e.g. http://example.org/ns#dim1"
+             "Malformed Cube. Every qb:Observation has exactly one associated qb:DataSet., e.g. http://example.org/ns#o1"))
+        (errors r))))
