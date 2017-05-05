9ac0706f10c38b2766f689b5c6cb7790c913d964
Robin Gower <robin.git@infonomics.ltd.uk>
2017-05-05 14:38:13 +0200

    Add PMD validation for multiple vocabularies in a graph

--- src/grafter/extra/validation/pmd.clj
+++ src/grafter/extra/validation/pmd.clj
@@ -1,7 +1,6 @@
 (ns grafter.extra.validation.pmd
   (:require [clojure.java.io :as io]
-            [grafter.extra.validation :refer [presence-checker]]
-            [grafter.extra.sparql :as sparql]))
+            [grafter.extra.validation :refer [example-finder presence-checker]]))
 
 (defn query-resource [filename]
   (->> filename (str "validation/pmd/") io/resource))
@@ -9,6 +8,10 @@
 (defn check-for-cube-vocab [repo]
   ((presence-checker (slurp (query-resource "has-cube-vocabulary.sparql")) "cube vocabulary missing") repo))
 
+(defn check-for-graph-with-multiple-vocabs [repo]
+  ((example-finder (slurp (query-resource "graph-with-multiple-vocabularies.sparql")) "graph containing more than one vocabulary") repo))
+
 (defn errors [repo]
-  (let [checks (list check-for-cube-vocab)]
+  (let [checks (list check-for-cube-vocab
+                     check-for-graph-with-multiple-vocabs)]
     (->> checks (map #(% repo)) (remove nil?))))
--- test/grafter/extra/validation/pmd_test.clj
+++ test/grafter/extra/validation/pmd_test.clj
@@ -1,5 +1,6 @@
 (ns grafter.extra.validation.pmd-test
   (:require [clojure.test :refer :all]
+            [grafter.rdf :refer [statements]]
             [grafter.extra.repository :refer [with-repository-containing]]
             [grafter.extra.validation.pmd :refer :all]))
 
@@ -7,10 +8,13 @@
   (some (partial re-find (re-pattern string)) strings))
 
 (deftest errors-test
-  (let [expected (list "cube vocabulary missing")
-        invalid (with-repository-containing [r "./test/resources/cube-bad.ttl"]
+  (let [expected (list "cube vocabulary missing"
+                       "graph containing more than one vocabulary, e.g. http://www.example.org/graph1")
+        invalid (with-repository-containing [r (mapcat statements ["./test/resources/cube-bad.ttl"
+                                                                   "./test/resources/graph-bad.trig"])]
                   (doall (errors r)))
-        valid (with-repository-containing [r "./test/resources/sdmx-dimension.ttl"]
+        valid (with-repository-containing [r (mapcat statements ["./test/resources/sdmx-dimension.ttl"
+                                                                 "./test/resources/graphs-good.trig"])]
                 (doall (errors r)))]
     (doseq [message expected]
       (is (includes? invalid message))
