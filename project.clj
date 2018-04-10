71067c9f96f5ec01df585732fc7ecabcecee5d3b
Robin Gower <robin.git@infonomics.ltd.uk>
2018-04-10 09:07:50 +0200

    Adopt grafter-0.11.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject grafter/extra "0.2.2-SNAPSHOT"
+(defproject grafter/extra "0.2.2-grafter-0.11.2-SNAPSHOT"
   :description "A collection of extra transformations and tools for developing Grafter pipelines"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [grafter/grafter "0.9.0"]
-                 [grafter/grafter.tabular "0.9.0"]
+                 [grafter/grafter "0.11.2"]
+                 [grafter/grafter.tabular "0.9.1-grafter-0.11.2-SNAPSHOT"]
                  [selmer "1.0.4"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]])
