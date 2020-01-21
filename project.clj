de0b43dd51f9a08444ee6fe2a202934c8c557114
Rick Moynihan <rick.m@swirrl.com>
2020-01-21 18:31:32 +0000

    Remove code that depends on grafter.tabular
    This will be moved into a new (but deprecated) repository.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [grafter/grafter "0.11.2"]
-                 [grafter/grafter.tabular "0.11.2"]
                  [selmer "1.0.4"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]])
--- src/grafter/extra/column.clj
+++
@@ -1,6 +0,0 @@
-(ns grafter.extra.column
-  (:require [grafter.tabular :refer [apply-columns]]
-            [grafter.sequences :refer [integers-from]]))
-
-(defn add-id-column [dataset column-name]
-  (apply-columns dataset {column-name (fn [_] (integers-from 0))}))
