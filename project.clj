cb1c21604fa4276591ca4fffc6ff16ee0e6075bc
Rick Moynihan <rick.m@swirrl.com>
2020-01-22 17:04:50 +0000

    Version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/extra "0.2.3-SNAPSHOT"
+(defproject grafter/extra "0.2.3"
   :description "A collection of extra transformations and tools for developing Grafter pipelines"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
